3b098b54b91f743fa8a023162abbaa44f4ae4fb0
David Halter <davidhalter88@gmail.com>
2013-08-19 17:22:59 +0430

    remove a strange comment

--- .vim/install.sh
+++ .vim/install.sh
@@ -3,7 +3,6 @@ git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 
-# Ubuntu installs don't need it because ropevim is enough
 # used for the Syntastic plugin (just for python)
 sudo aptitude install pyflakes
 # used for the pep8 plugin

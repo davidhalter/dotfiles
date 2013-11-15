0e8ff57d4fe0329b69d51bd8d81aae8116b5ebe8
David Halter <davidhalter88@gmail.com>
2013-11-15 15:21:51 +0100

    change dependencies from pyflakes (implicit install through flake8) to flake8

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,10 +3,6 @@ git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 
-# used for the Syntastic plugin (just for python)
-sudo aptitude install pyflakes
-# used for the pep8 plugin
-sudo aptitude install pep8
 # ctags is used for 
 sudo aptitude install exuberant-ctags
 
@@ -14,6 +10,5 @@ sudo aptitude install exuberant-ctags
 # anyway)
 sudo aptitude install python-pip
 
-sudo pip install jedi
-
+sudo pip install jedi flake8
 #sudo pip install vimpdb # doesn't work without vim-server

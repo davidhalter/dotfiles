88fc15b7a9ca811e666c274263ef3b97930834ca
Dave Halter <davidhalter88@gmail.com>
2023-01-31 09:52:05 +0100

    Add the pythonrc

---
+++ .pythonrc.py
@@ -0,0 +1,14 @@
+try:
+    from jedi.utils import setup_readline
+except ImportError:
+    # Fallback to the stdlib readline completer if it is installed.
+    # Taken from http://docs.python.org/2/library/rlcompleter.html
+    print("Jedi is not installed, falling back to readline")
+    try:
+        import readline
+        import rlcompleter
+        readline.parse_and_bind("tab: complete")
+    except ImportError:
+        print("Readline is not installed either. No tab completion is enabled.")
+else:
+    setup_readline()
